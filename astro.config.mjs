6323436ef5bd3624a08fca081ce1e7010a4b5f37
Calicolive <calicodeep@gmail.com>
2022-10-25 22:48:08 -0500

    added links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), solidJs()],
 });
